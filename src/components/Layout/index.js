9d870392f780666e968886ec7fe5f03cd241b7d4
Brendan Woo <woo.brendan@gmail.com>
2022-04-04 14:13:26 -0400

    add styling to layou t page. add in divs with spans to layout

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,10 +1,23 @@
 import Sidebar from '../Sidebar';
 import './index.scss';
+import { Outlet } from 'react-router-dom';
 
 export default function Layout() {
   return (
-    <>
+    <div className="App">
       <Sidebar />
-    </>
+      <div className="page">
+        <span className="tags top-tags">&lt;body&gt;</span>
+
+        <Outlet />
+
+        <span className='tags bottom-tag'>
+          &lt;/body&gt;
+          <br />
+          <span className='bottom-tag-html'>&lt;/html&gt;</span>
+
+        </span>
+      </div>
+    </div>
   )
 }
