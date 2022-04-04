85b0afdba9f89490d252194c4b6226b57b82096a
Brendan Woo <woo.brendan@gmail.com>
2022-04-04 14:39:32 -0400

    add home route

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import Layout from './components/Layout';
 import './App.scss';
 import {Routes, Route} from 'react-router-dom';
+import Home from './components/Home';
 
 function App() {
   return (
     <>
     <Routes>
       <Route path='/' element={<Layout />} />
+      <Route index element={<Home />} />
     </Routes>
     </>
   );
