17c501ffd27b688c60dc9e3193688e960c506988
Brendan Woo <woo.brendan@gmail.com>
2022-04-18 14:20:57 -0400

    fix import

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import Layout from './components/Layout';
 import './App.scss';
 import {Routes, Route} from 'react-router-dom';
-import Home from './components/Home';
+import Home from './components/Home/index';
 
 function App() {
   return (
